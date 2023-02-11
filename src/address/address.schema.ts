f7f38f9d9bbfc3c326f9127ab431b485a541c13b
Lyokolux <lyokolux@protonmail.com>
2023-02-11 19:44:16 +0100

    feat(address): add schema

---
+++ src/address/address.schema.ts
@@ -0,0 +1,24 @@
+import { Address } from '@prisma/client'
+import { Factory } from 'nestjs-seeder'
+
+export class AddressSchema implements Address {
+  id: number
+
+  @Factory(faker => faker?.address.buildingNumber().toString())
+    houseNumber: string
+
+  @Factory(faker => faker?.address.streetName())
+    streetName: string
+
+  @Factory(faker => faker?.address.zipCode())
+    zipCode: number
+
+  @Factory(faker => faker?.address.city())
+    city: string
+
+  @Factory(faker => faker?.address.longitude())
+    longitude: string
+
+  @Factory(faker => faker?.address.latitude())
+    latitude: string
+}
