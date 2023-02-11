5bdc3ccd43ddeb241747bc38f815826d22aa57a0
Lyokolux <lyokolux@protonmail.com>
2023-02-11 19:48:06 +0100

    feat(address): add seeder

--- src/seeder.ts
+++ src/seeder.ts
@@ -1,9 +1,10 @@
 import { seeder } from 'nestjs-seeder'
 import { PrismaModule } from './prisma/prisma.module'
+import { AddressSeeder } from './address/address.seeder'
 import { UserSeeder } from './user/user.seeder'
 
 seeder({
   imports: [
     PrismaModule
   ]
-}).run([UserSeeder])
+}).run([AddressSeeder, UserSeeder])
