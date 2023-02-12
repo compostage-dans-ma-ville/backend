4c8cd36341c7834a9c5299e80589d6578e90a929
BananeBoss <johannconicella@gmail.com>
2023-02-12 19:06:30 +0100

    init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 import { AppModule } from './user/app.module'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Composte Application API')
+    .setDescription('The composte application API description')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('doc', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
