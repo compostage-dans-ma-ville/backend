69c41e3a5c3e091b97a0a23b4b16ff0db8c5319d
Lyokolux <lyokolux@protonmail.com>
2023-02-15 21:59:32 +0100

    feat(pagination): add Links class

---
+++ src/services/pagination/dto/Links.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger'
+
+export class Links {
+  @ApiProperty({
+    description: 'Link to the previous page if this exists',
+    example:
+      'http://localhost:3000/api/v1/namespaces/1/configs?page=1&items=20'
+  })
+    prev?: string
+
+  @ApiProperty({
+    description: 'Link to the next page if this exists',
+    example:
+      'http://localhost:3000/api/v1/namespaces/1/configs?page=3&items=20'
+  })
+    next?: string
+
+  @ApiProperty({
+    description: 'Link to the last page',
+    example:
+      'http://localhost:3000/api/v1/namespaces/1/configs?page=5&items=20'
+  })
+    last: string
+
+  @ApiProperty({
+    description: 'Link to the first page',
+    example:
+      'http://localhost:3000/api/v1/namespaces/1/configs?page=1&items=20'
+  })
+    first: string
+}
