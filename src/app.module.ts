26074de0c8bf5c9899d46147e8394fadad9c0e14
Lyokolux <lyokolux@protonmail.com>
2023-02-15 22:08:54 +0100

    fix(app.module): move it into /src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { UserModule } from './user/user.module'
-import { PrismaModule } from './prisma/prisma.module'
-import { AuthModule } from './auth/auth.module'
+import { AppController } from '~/app.controller'
+import { AppService } from '~/app.service'
+import { PrismaModule } from '~/prisma/prisma.module'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './entities/user/user.module'
+import { SiteModule } from '~/entities/site/site.module'
+import { AddressModule } from '~/entities/address/address.module'
 
 @Module({
   imports: [
@@ -12,10 +14,13 @@ import { ConfigModule } from '@nestjs/config'
       isGlobal: true,
       envFilePath: '.env'
     }),
+    AddressModule,
+    AuthModule,
     UserModule,
     PrismaModule,
-    AuthModule],
+    SiteModule
+  ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, PrismaModule]
 })
 export class AppModule {}
