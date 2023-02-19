3a1c5b2340acb43b04f8e98d8e55d05f4c92fca2
Lyokolux <lyokolux@protonmail.com>
2023-02-19 21:07:17 +0100

    fix(seeding): path alias import with ts-node

--- src/seeder.ts
+++ src/seeder.ts
@@ -1,8 +1,9 @@
 import { seeder } from 'nestjs-seeder'
-import { PrismaModule } from './prisma/prisma.module'
-import { AddressSeeder } from './address/address.seeder'
-import { UserSeeder } from '~/user/user.seeder'
+import { AddressSeeder } from '~/address/address.seeder'
+import { PrismaModule } from '~/prisma/prisma.module'
 import { SiteSeeder } from '~/site/site.seeder'
+import { UserSeeder } from '~/user/user.seeder'
+
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
