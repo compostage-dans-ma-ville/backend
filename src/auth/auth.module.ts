8b778fd88ffb1b3e0f005cc6c16791e2e35cee5c
Lyokolux <lyokolux@protonmail.com>
2023-02-20 21:03:44 +0100

    refactor: update import path in order to use the alias

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AuthService } from './auth.service'
-import { AuthController } from './auth.controller'
-import { PrismaModule } from 'src/prisma/prisma.module'
-import { JwtModule } from '@nestjs/jwt'
-import { UserModule } from 'src/user/user.module'
 import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { PrismaModule } from '~/prisma/prisma.module'
+import { UserModule } from '~/user/user.module'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
 import { JwtStrategy } from './jwt.strategy'
 
 @Module({
