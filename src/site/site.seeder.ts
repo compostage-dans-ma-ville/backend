ce98f7d1d8965d1f24732ca98776534fc07dc34f
Lyokolux <lyokolux@protonmail.com>
2023-02-20 19:06:38 +0100

    fix(sites): make seeder work again

--- src/site/site.seeder.ts
+++ src/site/site.seeder.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common'
-import { Prisma } from '@prisma/client'
 import { Seeder, DataFactory } from 'nestjs-seeder'
 import { PrismaService } from '~/prisma/prisma.service'
 import { SiteSchema } from './site.schema'
@@ -12,10 +11,10 @@ export class SiteSeeder implements Seeder {
     const sites = DataFactory.createForClass(SiteSchema)
       .generate(10) //
     const siteCreations = await Promise.all(sites.map(async (s) => {
-      const { address } = s as unknown as SiteSchema
+      const { address, ...site } = s as unknown as SiteSchema
       const { id } = await this.prisma.address.create({ data: address })
       return {
-        ...s as Prisma.SiteCreateInput,
+        ...site,
         addressId: id
       }
     }))
