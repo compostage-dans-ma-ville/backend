a168ce8b519eb96d20e332e707ed5c17ff7ee50a
Lyokolux <lyokolux@protonmail.com>
2023-02-22 21:02:14 +0100

    test(sites): add a test for DELETE /sites/:id

--- test/sites.e2e-spec.ts
+++ test/sites.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { SiteModule } from '~/site/site.module'
 
-describe('GET /sites', () => {
+describe('sites', () => {
   let app: INestApplication
 
   beforeEach(async () => {
@@ -15,45 +15,65 @@ describe('GET /sites', () => {
     await app.init()
   })
 
-  it('returns a default pagination structure', async () => {
-    const { status, body } = await request(app.getHttpServer()).get('/sites')
+  describe('GET /sites', () => {
+    it('returns a default pagination structure', async () => {
+      const { status, body } = await request(app.getHttpServer()).get('/sites')
 
-    expect(status).toBe(200)
-    expect(body).toBeDefined()
-    expect(body).toEqual({
-      links: {
-        first: expect.any(String),
-        last: expect.any(String)
-      },
-      pagination: {
-        pageNumber: 1,
-        pageSize: 20,
-        totalCount: 20
-      },
-      data: expect.any(Array)
+      expect(status).toBe(200)
+      expect(body).toBeDefined()
+      expect(body).toEqual({
+        links: {
+          first: expect.any(String),
+          last: expect.any(String)
+        },
+        pagination: {
+          pageNumber: 1,
+          pageSize: 20,
+          totalCount: 20
+        },
+        data: expect.any(Array)
+      })
+    })
+
+    it('return a first site', async () => {
+      const { body } = await request(app.getHttpServer()).get('/sites')
+
+      expect(body.data[0]).toEqual(
+        {
+          id: expect.any(Number),
+          addressId: expect.any(Number),
+          avatar: null,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          name: expect.any(String),
+          description: null,
+          organizationId: null
+        }
+      )
+    })
+
+    it('return the expected amount of site', async () => {
+      const { body } = await request(app.getHttpServer()).get('/sites?items=10')
+
+      expect(body.data.length).toEqual(10)
     })
   })
 
-  it('return a first site', async () => {
-    const { body } = await request(app.getHttpServer()).get('/sites')
+  describe('DELETE /sites/:id', () => {
+    it('remove a site by id', async () => {
+      const { status, body } = await request(app.getHttpServer()).delete('/sites/8')
 
-    expect(body.data[0]).toEqual(
-      {
-        id: expect.any(Number),
+      expect(status).toBe(200)
+      expect(body).toMatchObject({
+        id: 8,
         addressId: expect.any(Number),
         avatar: null,
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
         name: expect.any(String),
-        description: null,
+        description: expect.any(String),
         organizationId: null
-      }
-    )
-  })
-
-  it('return the expected amount of site', async () => {
-    const { body } = await request(app.getHttpServer()).get('/sites?items=10')
-
-    expect(body.data.length).toEqual(10)
+      })
+    })
   })
 })
