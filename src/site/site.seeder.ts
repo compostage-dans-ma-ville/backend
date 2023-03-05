52515f91836862a8e36de65bc7f663599b6bfdc9
Lyokolux <lyokolux@protonmail.com>
2023-03-05 11:20:26 +0100

    feat(site.seeder.ts): seed a site with 0 or 1 schedule per day

--- src/site/site.seeder.ts
+++ src/site/site.seeder.ts
@@ -1,24 +1,50 @@
 import { Injectable } from '@nestjs/common'
 import { Seeder, DataFactory } from 'nestjs-seeder'
+import { randomInRange } from '~/api-services/utils'
+import { MINUTES_IN_DAY } from '~/api-services/weekTime'
 import { PrismaService } from '~/prisma/prisma.service'
+import { ScheduleSchema } from '~/schedule/schedule.schema'
 import { SiteSchema } from './site.schema'
+
+const DAYS_IN_WEEK = 7
+const getRandomOpeningInDay = (fromMin?: number): ScheduleSchema => {
+  const open = randomInRange(fromMin ?? 0, MINUTES_IN_DAY)
+  const close = randomInRange(open, MINUTES_IN_DAY)
+  return { open, close }
+}
 
 @Injectable()
 export class SiteSeeder implements Seeder {
   constructor(private prisma: PrismaService) {}
 
   async seed(): Promise<void> {
-    const sites = DataFactory.createForClass(SiteSchema)
+    const siteSeeds = DataFactory.createForClass(SiteSchema)
       .generate(20)
-    const siteCreations = await Promise.all(sites.map(async (s) => {
+    siteSeeds.forEach(async (s) => {
       const { address, ...site } = s as unknown as SiteSchema
       const { id } = await this.prisma.address.create({ data: address })
-      return {
-        ...site,
-        addressId: id
-      }
-    }))
-    await this.prisma.site.createMany({ data: siteCreations })
+
+      const schedules: ScheduleSchema[] = new Array(DAYS_IN_WEEK).fill(undefined)
+        .map((_, dayIndex) => {
+          const isClosedToday = Math.random() > 0.7
+          if (isClosedToday) return undefined
+          // const hasManyOpeningsToday = Math.random() > 0.7
+          // const amountOfOpenings = hasManyOpeningsToday ? randomInRange(1, 3) : 1
+          // TODO: insert many openings
+          return getRandomOpeningInDay(dayIndex * MINUTES_IN_DAY)
+        })
+        .filter((x: ScheduleSchema | undefined): x is ScheduleSchema => x !== undefined)
+
+      await this.prisma.site.create({
+        data: {
+          ...site,
+          Schedules: {
+            create: schedules
+          },
+          addressId: id
+        }
+      })
+    })
   }
 
   async drop(): Promise<void> {
