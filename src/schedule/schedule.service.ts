9dc878dc5aa11258eceb7446630249f2666d98d2
Lyokolux <lyokolux@protonmail.com>
2023-03-09 23:43:34 +0100

    feat(schedule.service.ts): add findAll and count methods

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -1,28 +1,48 @@
 import { Injectable } from '@nestjs/common'
-import { Schedule } from '@prisma/client'
-import { WeekTime } from '~/api-services/weekTime'
-import { GetScheduleDto } from './dto/get-schedule.dto'
+import { Opening, Prisma, Schedule } from '@prisma/client'
+import { PrismaService } from '~/prisma/prisma.service'
 
 @Injectable()
 export class ScheduleService {
-  toDto(schedules: Schedule[]): GetScheduleDto {
-    const scheduleDto: GetScheduleDto = [null, null, null, null, null, null, null]
-    schedules.forEach((schedule) => {
-      // TODO: is open the whole day. Use a boolean isOpenFullDay and isOpenFullDay ? []: [opening]
-      // TODO: write tests
-      const { days, hours, minutes } = WeekTime.fromMinutes(schedule.open)
-      const close = WeekTime.fromMinutes(schedule.close)
-      const opening = {
-        open: `${hours}:${minutes}`,
-        close: `${close.hours}:${close.minutes}`
-      }
-      if (scheduleDto[days] === null) {
-        scheduleDto[days] = [opening]
-      } else {
-        // can not be null as it is checked above
-        scheduleDto[days]!.push(opening)
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll({ siteId }: Prisma.ScheduleWhereInput): Prisma.PrismaPromise<(Schedule & {
+    openings: Opening[];
+  })[]> {
+    return this.prisma.schedule.findMany({
+      include: { openings: true },
+      where: {
+        siteId
       }
     })
-    return scheduleDto
   }
+
+  count({ siteId }: Prisma.ScheduleWhereInput): Prisma.PrismaPromise<number> {
+    return this.prisma.schedule.count({
+      where: {
+        siteId
+      }
+    })
+  }
+
+  // toDto(schedules: Schedue[]): GetScheduleDto {
+  //   const scheduleDto: GetScheduleDto = [null, null, null, null, null, null, null]
+  //   schedules.forEach((schedule) => {
+  //     // TODO: is open the whole day. Use a boolean isOpenFullDay and isOpenFullDay ? []: [opening]
+  //     // TODO: write tests
+  //     const { days, hours, minutes } = DailyTime.fromMinutes(schedule.open)
+  //     const close = DailyTime.fromMinutes(schedule.close)
+  //     const opening = {
+  //       open: `${hours}:${minutes}`,
+  //       close: `${close.hours}:${close.minutes}`
+  //     }
+  //     if (scheduleDto[days] === null) {
+  //       scheduleDto[days] = [opening]
+  //     } else {
+  //       // can not be null as it is checked above
+  //       scheduleDto[days]!.push(opening)
+  //     }
+  //   })
+  //   return scheduleDto
+  // }
 }
