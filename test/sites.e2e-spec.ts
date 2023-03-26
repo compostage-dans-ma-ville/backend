6f24b671705c935e462d681a8fabb4c1b9be0055
Lyokolux <lyokolux@protonmail.com>
2023-03-26 18:26:03 +0200

    test(sites): update test with potential null accessConditions

--- test/sites.e2e-spec.ts
+++ test/sites.e2e-spec.ts
@@ -47,7 +47,7 @@ describe('sites', () => {
         launchDate: null,
         name: expect.any(String),
         description: expect.any(String),
-        accessConditions: expect.any(String),
+        accessConditions: expect.toSatisfy(e => e === null || typeof e === 'string'),
         isPublic: expect.any(Boolean),
         Address: {
           id: expect.any(Number),
