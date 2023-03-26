b8aa5eb19157f52e948176eb183c7ceaaa20694e
Lyokolux <lyokolux@protonmail.com>
2023-03-26 10:38:04 +0200

    feat(GetAddress.dto.ts): expose the attributes

--- src/address/dto/GetAddress.dto.ts
+++ src/address/dto/GetAddress.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Address } from '@prisma/client'
+import { Expose } from 'class-transformer'
 
 export class GetAddressDto implements Address {
   @ApiProperty({
@@ -8,36 +9,42 @@ export class GetAddressDto implements Address {
   })
     id: number
 
+  @Expose()
   @ApiProperty({
     description: 'Number associated with the street',
     example: '6 ter'
   })
     houseNumber: string
 
+  @Expose()
   @ApiProperty({
     description: 'Last update date of this site.',
     example: 'rue Lothaire'
   })
     streetName: string
 
+  @Expose()
   @ApiProperty({
     description: 'Human friendly name of this site.',
     example: 67150
   })
     zipCode: number
 
-    @ApiProperty({
-      description: 'The city or village name of the address.',
-      example: 'Erstein'
-    })
-      city: string
+  @Expose()
+  @ApiProperty({
+    description: 'The city or village name of the address.',
+    example: 'Erstein'
+  })
+    city: string
 
+  @Expose()
   @ApiProperty({
     description: 'the exact latitude in legal projection.',
     example: 1044994.79
   })
     longitude: number
 
+  @Expose()
   @ApiProperty({
     description: 'the exact longitude in legal projection.',
     example: 6824079.46
