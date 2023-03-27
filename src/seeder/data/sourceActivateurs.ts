371f1a50d48a2fabefa0dd414c08d339fe9238a4
Lyokolux <lyokolux@protonmail.com>
2023-03-27 22:13:42 +0200

    feat(seeding): add a type for the whole JSON

--- src/seeder/data/sourceActivateurs.ts
+++ src/seeder/data/sourceActivateurs.ts
@@ -4,51 +4,10 @@ export type BoundedBy ={
   }
 }
 
-export type BoundedBy3 ={
-  Box: {
-    coordinates: string
-  }
-}
+type Quantity = 'inf_5_t' | '5_a_10_t' | '11_a_20_t' | '21_a_30_t' | '31_a_52_t' | 'ne_sait_pas'
 
-export type SiteCompostage2 ={
-  boundedBy: any
-  geometry: any
-  id_site: any
-  libelle: any
-  type: any
-  adresse: any
-  ville: any
-  code_postal: any
-  is_public: any
-  is_signataire_charte: any
-  quantite_traitee: any
-  nb_contributeurs: any
-  nb_repas_annuel: any
-  type_materiel: any
-  exploitant_nom: any
-  exploitant_structure: any
-  exploitant_contact: any
-  contact_referent: any
-  acteur: any
-  date_maj_fiche: any
-  date_mise_en_route: any
-  photo: any
-  condition_acces: any
-  fonctionnement_site: any
-  nom_dept: any
-  code_dept: any
-  nom_reg?: string
-  code_reg: any
-}
-
-export type FeatureMember2 ={
-  v_site_compostage_dept_3857: SiteCompostage2
-}
-
-export type FeatureCollection2 ={
-  boundedBy: BoundedBy3
-  featureMember: FeatureMember2[]
-}
+type SiteType = 'autre_dechets_verts' | 'autre_etablissement' | 'autre_partage' | 'EHPAD' | 'etab_touristique' | 'ferme' | 'milieu_hospitalier' | 'ne sait pas' | null | 'pied_immeuble' | 'plateforme_communale' | 'quartier' | 'resto_administratif' | 'resto_entreprise' | 'scolaire' | 'vermicompostage'
+type ContributorAmount = number | `${number} ${'F' | 'f'}Foyers` | '' | '/' | '?' | '20 à 30 foyers' | '25 foyers / 240' | '270 apporteurs de biodéchets (mars 2022)'| '30+'| '40-50'| '40foyers' | '40 max'| '45 environ'| '45 - variable'| '5/6 Foyers' |'cantine scolaire' | 'Entre 10 et 20 foyers par semaine' | 'Inconnu' | 'nb de participants : 120' | 'nb de participants : 50' | 'Nombre de foyers participants : 80\nAutonomie en broyat : Oui' | 'Non connu' | 'Une dizaine'
 
 export type SiteCompostage ={
   boundedBy?: {
@@ -61,42 +20,31 @@ export type SiteCompostage ={
       coordinates: string
     }
   }
-  id_site?: number
-  libelle?: string
-  type?: string
-  adresse?: string
+  id_site: number
+  libelle: string
+  type: SiteType
+  adresse: string
   ville: string
   code_postal: number
-  is_public?: 'oui' | 'non'
-  is_signataire_charte?: string
-  quantite_traitee?: 'ne_sait_pas'
-  nb_contributeurs: string
-  nb_repas_annuel: string
-  type_materiel?: string
-  exploitant_nom?: string
-  exploitant_structure?: string
-  exploitant_contact: string
-  contact_referent: string
-  acteur?: string
-  date_maj_fiche?: string
+  is_public: 'oui' | 'non' | 'ne sait pas' | 'ne_sait_pas'
+  is_signataire_charte: string
+  quantite_traitee: Quantity
+  nb_contributeurs: ContributorAmount
+  nb_repas_annuel: number | '' | '?' | 'cantine scolaire également' | 'Environ 400' | 'Inconnu' | 'Non connu'
+  type_materiel: string // TODO: clarify this complexity
+  exploitant_nom: string // sometimes prefixed with "Référent(s): "
+  exploitant_structure: string
+  exploitant_contact: string // "email / phone" | "Name phone" | "email / phone" | string
+  contact_referent: string // same mess as exploitant_contact
+  acteur: string // also a mess
+  /** YYYY-MM-DD */
+  date_maj_fiche: string
   date_mise_en_route: string
-  photo?: string
-  condition_acces?: string
-  fonctionnement_site?: string
-  nom_dept?: string
-  code_dept: number
-  nom_reg?: string
+  photo: string
+  condition_acces: string
+  fonctionnement_site: string
+  nom_dept: string
+  code_dept: string | number
+  nom_reg: string
   code_reg: number
-  FeatureCollection?: FeatureCollection2
-}
-
-export type FeatureMember = {
-  v_site_compostage_dept_3857: SiteCompostage
-}
-
-export type Root ={
-  FeatureCollection: {
-    boundedBy: BoundedBy
-    featureMember: FeatureMember[]
-  }
 }
