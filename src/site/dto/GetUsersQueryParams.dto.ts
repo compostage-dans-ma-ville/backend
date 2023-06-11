537f6ecb7b4b8fadad2398b2edfaf4d80b78b235
Lyokolux <lyokolux@protonmail.com>
2023-06-11 17:13:12 +0200

    style(users): clean getUsersQueryParams dto

--- src/site/dto/GetUsersQueryParams.dto.ts
+++ src/site/dto/GetUsersQueryParams.dto.ts
@@ -1,5 +1,3 @@
-import { IntersectionType } from '@nestjs/mapped-types'
-import { CoordsQueryParams } from '~/address/dto/CoordsQueryParams.dto'
 import { PaginationQueryParams } from '~/api-services/pagination/dto/PaginationQueryParams'
 
 export class GetSitesQueryParams
